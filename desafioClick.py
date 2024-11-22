5e97c9c9304ccfe644bf46a4ceb08001291e7459
Edgardo Merino <edgardomerino@live.com>
2024-11-22 17:30:45 -0300

    desafio criar pastas

---
+++ desafioClick.py
@@ -0,0 +1,7 @@
+import pyautogui
+pyautogui.click(x=1084,y=364, clicks=1,button='right',interval=0.1,duration=0.3)
+pyautogui.move(10,280,duration=0.3)
+pyautogui.move(300,0,duration=1)
+pyautogui.leftClick()
+pyautogui.leftClick()
+
