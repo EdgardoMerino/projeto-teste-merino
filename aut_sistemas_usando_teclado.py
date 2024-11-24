ba8a2728cc56e285892adb18921cf6136e79d5a8
Edgardo Merino <edgardomerino@live.com>
2024-11-24 13:47:05 -0300

    usar teclado para preenchimento

--- aut_sistemas_usando_teclado.py
+++ aut_sistemas_usando_teclado.py
@@ -10,3 +10,13 @@ sleep(1)
 pyautogui.press('tab')
 sleep(1)
 pyautogui.press('space')
+
+#dicas:
+print(pyautogui.KEYBOARD_KEYS)
+
+#copiar texto e colar
+pyautogui.click(518,283,duration=1)
+pyautogui.hotkey('ctrl', 'a')
+pyautogui.hotkey('ctrl','c')
+pyautogui.click(428,183,duration=1)
+pyautogui.hold('ctrl', 'v')
